67d7a51221f24bfa205db1781cc706fdb3969a96
roverino <jr.tombolini@gmail.com>
2019-07-26 18:18:52 -0400

    Now with consistency !

--- src/rovergame/Game.java
+++ src/rovergame/Game.java
@@ -32,7 +32,10 @@ public class Game implements Runnable {
 		
 	}
 	
+	int x=0;
+	
 	private void tick() {
+		x += 1;
 		
 	}
 	
@@ -48,10 +51,11 @@ public class Game implements Runnable {
 		
 		g.drawImage(Assets.map, -1750, -1226, null);
 		g.drawImage(Assets.player, 300, 300, null);
-		g.drawImage(Assets.rock1, 0, 0, null);
+		g.drawImage(Assets.rock1, x, 0, null);
 		g.drawImage(Assets.rock1, 100, 100, null);
 		g.drawImage(Assets.rock2, 300, 150, null);
 		g.drawImage(Assets.rock3, 500, 600, null);
+
 
 		
 		
@@ -61,12 +65,44 @@ public class Game implements Runnable {
 	}
 	
 	public void run() {
+		
 		init();
 		
+		//this is to stabilize frames per second, on slow/medium/fast computers
+		int fps = 60; //max frames per second
+		double timePerTick = 1000000000/fps;
+		// this is 1 second (in nanoseconds) divided by frames per second
+		// this equals maximum amount of time allowed to have to perform the 60fps action
+		// max amount of time to run tick and render methods
+		double delta = 0; //times we need to render to meet fps standards
+		long now; //initialized in our game loop 
+		long lastTime = System.nanoTime(); //current computer time
+		long timer = 0; //time until we get to 1 second (so we can print our fps)
+		int ticks = 0; //how many ticks were in a second (fps)
+		
 		while (running) {
+			now = System.nanoTime(); //current time of our computer
+			delta += (now - lastTime) / timePerTick; 
+			//delta = adds (amount of time passed since this line was run since the last loop)/(max amount of alotted time)
+			//delta = how much time we have until we must call tick and render again (bc frame cap)
+			timer += now - (lastTime); //adding amount of time since above block was called?
+			lastTime = now; //current time at the time this block was run
+			
+			if(delta >= 1) { //if delta is 1 or greater, that means we need to tick and render to achieve 60fps	
 			tick();
 			render();
+			ticks++;
+			delta--; //meaning we ticked and rendered so we can minus by 1
+			}
+			
+			if (timer >= 1000000000) { //shows fps every second
+				System.out.println("Ticks and Frames: " + ticks);
+				ticks = 0;
+				timer = 0; //these two = 0 to reset and constantly give us the ticks/sec
+			}
 		}
+		
+		stop();
 		
 	}
 	public synchronized void start() {
